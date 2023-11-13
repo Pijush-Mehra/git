f48009068f1dc4dbb102b7a859d4fe9aec4c7f78
Pijush Kumar Mehra <Email>
2023-11-13 15:40:51 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { title: 'Express' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
+router.get('/address', function(req, res, next) {
+  res.render('address', { title: 'Express' });
+});
+router.get('/paymentt', function(req, res, next) {
+  res.render('paymentt', { title: 'Express' });
+});
+
+module.exports = router;
